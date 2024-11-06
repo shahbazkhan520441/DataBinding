5d7a7774f97beaa1765fa19759813bea11921275
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-11-06 11:40:27 +0530

    interploation binding 2.property binding 3.event binding 4. two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModel } from '@angular/forms';
+import { FormsModule
 
+
+ } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DataBinding';
+
+  public name="mr shahbaz khan for learning data binding properly "
+
+  public role="java full stack developer"
+
+  public image="/public/book.jpg"
+ 
+   public value =""
+
+  onClick(){
+    console.log("thank you for subscribing")
+  }
+  
+ 
 }
